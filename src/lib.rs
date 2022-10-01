b5d347907838335032275b8e14a7c76983df7b01
Psiprobe <1062875775ycx@gmail.com>
2022-10-01 13:23:13 +0800

    Update

--- src/lib.rs
+++ src/lib.rs
@@ -1180,10 +1180,10 @@ impl State {
                     ops: wgpu::Operations {
                         load: wgpu::LoadOp::Clear(
                             wgpu::Color {
-                                r: 1.0,
-                                g: 1.0,
-                                b: 1.0,
-                                a: 1.0,
+                                r: 0.0,
+                                g: 0.0,
+                                b: 0.0,
+                                a: 0.0,
                             }
                         ),
                         store: true,
