f1b151d0fdfffcb2eb2498e307566a8d6d2eaf6b
AIClink <2353726394@qq.com>
2022-10-25 17:25:05 +0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -54,12 +54,12 @@ struct Vertex {
 
 const VERTICES: &[Vertex] = &[
 
-    Vertex { position: [25.0,   -1.0,  25.0],      color: [0.7, 0.7, 0.7 ,1.0],       normal:[0.0, 1.0, 0.0],   },
+    Vertex { position: [25.0,   -1.0,  25.0],      color: [0.55, 1.0, 0.7 ,1.0],       normal:[0.0, 1.0, 0.0],   },
     Vertex { position: [0.0,    -1.0, 25.0],      color: [0.7, 0.7, 0.7 ,1.0],       normal:[0.0, 1.0, 0.0],   },
     Vertex { position: [25.0,   -1.0, 0.0],      color: [0.7, 0.7, 0.7 ,1.0],       normal:[0.0, 1.0, 0.0],   },
     Vertex { position: [25.0,   -1.0, 0.0],      color: [0.7, 0.7, 0.7 ,1.0],       normal:[0.0, 1.0, 0.0],   },
     Vertex { position: [0.0,    -1.0, 25.0],      color: [0.7, 0.7, 0.7 ,1.0],       normal:[0.0, 1.0, 0.0],   },
-    Vertex { position: [0.0,    -1.0, 0.0],        color: [0.7, 0.7, 0.7 ,1.0],       normal:[0.0, 1.0, 0.0],   },
+    Vertex { position: [0.0,    -1.0, 0.0],        color: [1.0, 0.2, 0.2 ,1.0],       normal:[0.0, 1.0, 0.0],   },
 
     Vertex { position: [0.0,    -1.0,  25.0],      color: [0.3, 0.3, 0.3 ,1.0],       normal:[0.0, 1.0, 0.0],   },
     Vertex { position: [-25.0,  -1.0, 25.0],      color: [0.3, 0.3, 0.3 ,1.0],       normal:[0.0, 1.0, 0.0],   },
